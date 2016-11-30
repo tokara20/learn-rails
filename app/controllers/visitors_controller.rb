26d0d8c9883535b7be8095cc582361357d565120
Tokara <tokara20@yandex.com>
2016-11-30 07:15:57 +0000

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
     #render 'visitors/new'
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My Birthday is soon.'
   end
 end
 
