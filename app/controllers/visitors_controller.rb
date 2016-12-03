690072046b87e564f18193c22fefa006af053ffc
Tokara <tokara20@yandex.com>
2016-12-03 02:14:56 +0000

    mailist list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,24 @@
 class VisitorsController < ApplicationController
-  def new
-    @owner = Owner.new
-    #render 'visitors/new'
-    #flash.now[:notice] = 'Welcome!'
-    #flash.now[:alert] = 'My Birthday is soon.'
-  end
-end
 
+  def new
+    @visitor = Visitor.new
+  end
+
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:email)
+  end
+
+end
